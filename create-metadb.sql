175b0988c16f6420aff9adee2e2041e609c45cb2
Yifeng Jiang <uprushworld@gmail.com>
2017-01-20 10:54:21 +0900

    fix postgresql setup script

--- create-metadb.sql
+++ create-metadb.sql
@@ -1,14 +1,14 @@
-# for hive
+-- for hive
 CREATE USER hive WITH PASSWORD 'myPassword';
 CREATE DATABASE hive;
 GRANT ALL PRIVILEGES ON DATABASE hive to hive;
 
-# for oozie
+-- for oozie
 CREATE USER oozie WITH PASSWORD 'myPassword';
 CREATE DATABASE oozie;
 GRANT ALL PRIVILEGES ON DATABASE oozie to oozie;
 
-# for ranger
+-- for ranger
 CREATE USER rangeradmin WITH PASSWORD 'myPassword';
 CREATE DATABASE ranger;
 GRANT ALL PRIVILEGES ON DATABASE ranger to rangeradmin;
--- setup-postgre.sh
+++ setup-postgre.sh
@@ -18,18 +18,30 @@ fi
 cp ${PG_CONF} ${PG_CONF}.backup
 
 # create DB and user for Hive, Ranger and Oozie
-sed 's/myPassword/${MASTER_PWD}/g' $DEMO_HOME/create-metadb.sql > /tmp/hdemo-create-metadb.sql
-su -u postgres psql -a -f /tmp/hdemo-create-metadb.sql
+sed "s/myPassword/${MASTER_PWD}/g" $DEMO_HOME/create-metadb.sql > /tmp/hdemo-create-metadb.sql
+su - postgres -c "psql -a -f /tmp/hdemo-create-metadb.sql"
 rm -f /tmp/hdemo-create-metadb.sql
 
+echo 'local  ranger  rangeradmin md5' >> ${PG_CONF}
 echo 'host  ranger   rangeradmin 0.0.0.0/0  md5' >> ${PG_CONF}
+echo 'host  ranger   rangeradmin ::/0 md5' >> ${PG_CONF}
+
+echo 'local  hive   hive md5' >> ${PG_CONF}
 echo 'host  hive   hive 0.0.0.0/0  md5' >> ${PG_CONF}
+echo 'host  hive   hive ::/0 md5' >> ${PG_CONF}
+
+echo 'local oozie   oozie md5' >> ${PG_CONF}
 echo 'host  oozie   oozie 0.0.0.0/0  md5' >> ${PG_CONF}
+echo 'host  oozie   oozie ::/0 md5' >> ${PG_CONF}
 
 
 # allow postgres user to connect using password
-su -u postgres psql -c "alter user postgres password '${MASTER_PWD}';"
+su - postgres -c "psql -c 'alter user postgres password '${MASTER_PWD}';'"
 
 sed -i 's/host\s*all\s*postgres\s*127\.0\.0\.1\/32\s*ident/host    all   postgres             127.0.0.1\/32            md5/' /var/lib/pgsql/data/pg_hba.conf
 sed -i 's/host\s*all\s*postgres\s*::1\/128\s*ident/host    all   postgres             ::1\/128                 md5/' /var/lib/pgsql/data/pg_hba.conf
 service postgresql reload
+
+# some test
+su - postgres -c "psql -c '\l'"
+#psql hive -U hive -W -p 5432 -h localhost -c "\l"

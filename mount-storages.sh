0c02051dde7ea12b66e0b57e89a10d732d009a23
Yifeng Jiang <uprushworld@gmail.com>
2017-02-27 17:30:35 +0800

    clean

--- mount-storages.sh
+++ mount-storages.sh
@@ -1,45 +1,65 @@
 #!/bin/bash
 
-apt install -y xfsprogs
+DEMO_HOME=`dirname $0`
+DEMO_HOME=`cd $DEMO_HOME; pwd`
 
-umount /mnt
-sed -i '/xvdb/d' /etc/fstab
+source $DEMO_HOME/functions.sh
 
+function prepare() {
+  if [[ "$OS_FAMILY" == "REDHAT" ]]; then
+    echo "TODO"
+  else
+    apt install -y xfsprogs
+    umount /mnt
+    sed -i '/xvdb/d' /etc/fstab
+  fi
+}
 
-for DEVICE in {b..g};
-do
-  echo "making xfs on device $DEVICE"
-  mkfs.xfs -K /dev/xvd${DEVICE}
-done
+function make_fs() {
+  if [[ "$OS_FAMILY" == "REDHAT" ]]; then
+    echo "TODO"
+  else
+    for DEVICE in {b..g};
+    do
+      echo "making xfs on device $DEVICE"
+      mkfs.xfs -K /dev/xvd${DEVICE}
+    done
+  fi
 
-echo "Done making XFS"
-echo
+  echo "Done making XFS"
+  echo
+}
 
-for i in {0..5}
-do
-  echo "making mount point ${i}"
-  mkdir -p /grid/${i}
-done
-echo "Done making mount points"
+function mount_fs() {
+  for i in {0..11}
+  do
+    echo "making mount point ${i}"
+    mkdir -p /grid/${i}
+  done
+  echo "Done making mount points"
 
+  GRID=0
+  for DEVICE in {b..g};
+  do
+    echo "adding entry to fstab for device $DEVICE"
+    echo "/dev/xvd${DEVICE} /grid/${GRID} xfs noatime,nodiratime 1 2" >> /etc/fstab
 
-GRID=0
-for DEVICE in {b..g};
-do
-  echo "adding entry to fstab for device $DEVICE"
-  echo "/dev/xvd${DEVICE} /grid/${GRID} xfs noatime,nodiratime 1 2" >> /etc/fstab
+    GRID=$((GRID+1))
+  done
+  echo "Done adding fstab entries"
+  echo
+  echo "Mount all XFS"
 
-  GRID=$((GRID+1))
-done
-echo "Done adding fstab entries"
-echo
-echo "Mount all XFS"
+  mount -a
 
-mount -a
+  echo "DONE mounting all devices"
+  cat /etc/fstab
 
-echo "DONE mounting all devices"
-cat /etc/fstab
+  echo
+  df -h
 
-echo
-df -h
+}
 
+prepare
+make_fs
+mount_fs
